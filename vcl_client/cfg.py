e025da8d6af8ebad2844951e41dad2ba6cd92fc5
Ryan Hefner <ryan.hefner@netapp.com>
2015-06-27 01:54:11 -0400

    Implement `boot`
    Enables user to run boot with an image ID and start a VM.

--- vcl_client/cfg.py
+++ vcl_client/cfg.py
@@ -16,11 +16,13 @@ USERNAME_KEY = 'username'
 CONF = ConfigParser.ConfigParser()
 CONF.read(CONFIG_FILE_PATH)
 
+
 def initialize_config():
     """Sets the initial config file options."""
     if not CONF.has_section(CONF_SECTION):
         CONF.add_section(CONF_SECTION)
         write_conf(BASE_URL_KEY, DEFAULT_ENDPOINT)
+
 
 def get_conf(key):
     """Gets a key from the config file"""
@@ -45,5 +47,6 @@ def get_password():
         return keyring.get_password('system', get_conf(USERNAME_KEY))
     except AttributeError:
         return None
+
 
 initialize_config()
