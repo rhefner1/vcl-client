3e23534106ab1524c9fee944560caf9d59e4b32c
Ryan Hefner <ryan.hefner@netapp.com>
2015-07-04 20:40:04 -0400

    Check status after creating request
    After creating a request, the client should check to see if the
    reservation is available and let the user know when it is. This
    can optionally be turned off with a flag.
    
    Closes #5

--- tests/fakes.py
+++ tests/fakes.py
@@ -6,9 +6,31 @@ PASSWORD = 'password'
 
 JSON_RETURN = '[{"ID": 123, "Name": "test"}]'
 
-XML_RETURN = """<?xml version='1.0'?>
-<methodCall>
-<methodName>XMLRPCgetTest</methodName>
-</methodCall>"""
+XML_RETURN = """<?xml version="1.0" encoding="iso-8859-1"?>
+<methodResponse>
+<params>
+ <param>
+  <value>
+   <struct>
+    <member>
+     <name>status</name>
+     <value>
+      <string>success</string>
+     </value>
+    </member>
+    <member>
+     <name>requests</name>
+     <value>
+      <array>
+       <data/>
+      </array>
+     </value>
+    </member>
+   </struct>
+  </value>
+ </param>
+</params>
+</methodResponse>
+"""
 
-XML_PARSED = ((), u'XMLRPCgetTest')
+XML_PARSED = {'status': 'success', 'requests': []}
